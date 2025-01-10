e6e79ae1fba08562382004edd2921e03bc67764c
RubanKumar13 <rubanraav13@gmail.com>
2025-01-10 10:51:13 +0530

    configureroute js file

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,26 +1,62 @@
+// import connectMango from "../../../utils/connect";
+// import PostModel from "../../../models/postModel";
+
+// export async function GET(req){
+//     const query =  req.nextUrl.searchParams.get('q')
+//     try{
+//         await connectMango();
+//         let postdata;
+//         if(query){
+//              postdata =  await PostModel.find({
+//                 $or : [
+//                     {title: new RegExp(query,'i')},
+//                     {description: new RegExp(query,'i')}
+//                 ]
+//             });
+            
+//         } else{
+
+//              postdata =  await PostModel.find({});
+//         }
+//         return Response.json(postdata);
+//     } catch(error){
+//         return Response.json({message:error.message})
+//     }
+   
+// }
 import connectMango from "../../../utils/connect";
 import PostModel from "../../../models/postModel";
 
-export async function GET(req){
-    const query =  req.nextUrl.searchParams.get('q')
-    try{
+export async function GET(req) {
+    const query = req.nextUrl.searchParams.get('q');
+
+    try {
+        // Connect to the MongoDB database
         await connectMango();
+
+        // Fetch post data based on query
         let postdata;
-        if(query){
-             postdata =  await PostModel.find({
-                $or : [
-                    {title: new RegExp(query,'i')},
-                    {description: new RegExp(query,'i')}
+        if (query) {
+            postdata = await PostModel.find({
+                $or: [
+                    { title: new RegExp(query, 'i') },
+                    { description: new RegExp(query, 'i') }
                 ]
             });
-            
-        } else{
-
-             postdata =  await PostModel.find({});
+        } else {
+            postdata = await PostModel.find({});
         }
-        return Response.json(postdata);
-    } catch(error){
-        return Response.json({message:error.message})
+
+        // Return success response
+        return new Response(JSON.stringify(postdata), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        // Return error response
+        return new Response(JSON.stringify({ message: error.message }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
-   
 }
