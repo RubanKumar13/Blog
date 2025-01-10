bac70aa0724982b3b5c31e86e37006eb8c1695f5
RubanKumar13 <rubanraav13@gmail.com>
2025-01-10 12:11:10 +0530

    credits add at footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,10 @@ export default function Footer() {
       <>
         <footer className="fixed inset-x-0 flex flex-col items-center bg-neutral-900 text-center text-white">
             <div className="w-full p-4 text-center" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
-                 2025 Blog App
+                 2025 Blog App 
+            </div>
+            <div className="w-full p-4 text-center" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
+              Developed by Rubankumar&hearts;
             </div>
 
         </footer>
