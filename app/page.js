289f21fdab2b12ce70f82692622590f0f24952db
RubanKumar13 <rubanraav13@gmail.com>
2025-01-11 15:55:21 +0530

    ui changes

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
         <h2 className="text-4xl font-bold mb-4">Welcome to Our Blog</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </main>
-    <div className="flex justify-end px-4">
+    <div className="flex justify-end px-4 mb-3">
     <input ref={inputRef} type="text" disabled={search} onKeyDown={searchPost} className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
     <button onClick={searchPost} disabled={search} className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">{search?'...':'Search'}</button>
   </div>
--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,11 @@
 export default function Footer() {
     return (
       <>
-        <footer className="fixed inset-x-0 bottom-0 flex flex-col items-center bg-neutral-900 text-center text-white">
-            <div className="w-full p-4 text-center" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
-                 2025 Blog App 
+        <footer className="fixed inset-x-0 bottom-0 flex flex-col items-center bg-white shadow-lg text-center ">
+            <div className="w-full p-4 text-center font-bold text-red-500 " >
+                 2025 Blog App | | Developed by Rubankumar&#x2665;
             </div>
-            <div className="w-full p-4 text-center" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
-              Developed by Rubankumar&hearts;
-            </div>
+           
 
         </footer>
       </>
